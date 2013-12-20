688ff7e38ea3c7754719162b809b3efac8daca08
NicoSantangelo <santangelonicolas@gmail.com>
2013-12-20 08:44:31 -0300

    Show the text of previously stored keys. Extract the logic to yaml.py

--- i18n_rails.py
+++ i18n_rails.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin
-from . import pyyaml
 from .locales_path import LocalesPath
+from .yaml import Yaml
 
 class I18nRailsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -28,7 +28,6 @@ class I18nRailsCommand(sublime_plugin.TextCommand):
             # Prompt an input to place the translation foreach language
             self.process()
 
-
     def process(self, user_text = None):
         # Write the files keeping in mind the presence (or lack of) a dot to place the keys in the yml
         if user_text:
@@ -36,57 +35,20 @@ class I18nRailsCommand(sublime_plugin.TextCommand):
 
         locale = self.locales_path.process()
         if locale:
-            self.show_input_panel(locale, self.process, None, self.process)
+            existing_text = self.existing_text_from_yaml()
+            self.show_input_panel(locale, existing_text, self.process, None, self.process)
+
+    def existing_text_from_yaml(self):
+        self.yaml = Yaml(self.locales_path)
+        return self.yaml.text_from(self.selected_text)
 
     def write_text(self, text):
-        # Get the path of the yml file
-        yaml_path = self.locales_path.yaml()
+        self.yaml.write_text(text)
 
-        # Transform it to an dic
-        with open(yaml_path, 'r+') as yaml_file:
-            yaml_dict = yaml_to_write = pyyaml.load(yaml_file)
+        self.display_message("{0}: {1} created!".format(self.selected_text, text))
 
-            # Find the full paths file name key on the dict inside 
-            keys = [ self.locales_path.locale_name() ] # root: es|en|...
-
-            if self.selected_text.startswith("."):
-                # [model, child]
-                keys += [self.locales_path.path.modelname(), self.locales_path.file_name()]
-
-                # Remove the dot    
-                last_key = self.selected_text[1:]
-            else:
-                #[ key1, key2, ...]
-                keys += self.selected_text.split(".")
-                last_key = keys.pop()
-
-            # Move on the yaml file
-            yaml_dict = self.traverse_yml(yaml_dict, keys)
-
-            # Add the selected text as a new key inside the file_name key with the text as a value
-            yaml_dict[last_key] = text
-
-            # Save the file
-            self.write_yaml_file(yaml_file, yaml_to_write)
-
-            self.display_message("{0}: {1} created!".format(last_key, text))
-
-    def traverse_yml(self, yaml_dict, keys):
-        for key in keys:
-            if not key in yaml_dict or yaml_dict[key] is None:
-                yaml_dict[key] = {}
-
-            yaml_dict = yaml_dict[key]
-
-        return yaml_dict
-
-    def write_yaml_file(self, yaml_file, data_to_write):
-        yaml_file.seek(0)
-        yaml_file.write( pyyaml.dump(data_to_write, default_flow_style = False, allow_unicode = True, encoding = None) )
-        yaml_file.truncate()
-
-    def show_input_panel(self, message, on_done = None, on_change = None, on_cancel = None):
-        self.view.window().show_input_panel(message, "", on_done, on_change, on_cancel)
+    def show_input_panel(self, caption, initial_text = "", on_done = None, on_change = None, on_cancel = None):
+        self.view.window().show_input_panel(caption, initial_text, on_done, on_change, on_cancel)
 
     def display_message(self, text):
       sublime.active_window().active_view().set_status("i18_rails", text)
---
+++ yaml.py
@@ -0,0 +1,66 @@
+from . import pyyaml
+
+class Yaml():
+    def __init__(self, locales_path):
+        self.locales_path = locales_path
+        self.reset()
+
+    def move_to(self, selected_text):
+        # Find the full paths file name key on the dict inside 
+        keys = [ self.locales_path.locale_name() ] # root: es|en|...
+
+        if selected_text.startswith("."):
+            # [model, child]
+            keys += [self.locales_path.path.modelname(), self.locales_path.file_name()]
+
+            # Remove the dot    
+            self.last_key = selected_text[1:]
+        else:
+            #[ key1, key2, ...]
+            keys += selected_text.split(".")
+            self.last_key = keys.pop()
+
+        # Move on the yaml file
+        self.traverse(keys)
+
+        return self.dict
+
+    def text_from(self, selected_text):
+        self.read_file()
+
+        self.move_to(selected_text)
+
+        return self.dict[self.last_key] if self.last_key in self.dict else ""
+
+    def write_text(self, text):
+        with open(self.locales_path.yaml(), 'w') as yaml_file:
+            self.dict[self.last_key] = text
+
+            self.write_file(yaml_file)
+
+        return self
+
+    def read_file(self):
+        with open(self.locales_path.yaml(), 'r') as yaml_file:
+            self.dict = self.yaml_to_write = pyyaml.load(yaml_file)
+
+        return self.dict
+
+    def write_file(self, yaml_file):
+        yaml_file.seek(0)
+        yaml_file.write( pyyaml.dump(self.yaml_to_write, default_flow_style = False, allow_unicode = True, encoding = None) )
+        yaml_file.truncate()
+
+    def traverse(self, keys):
+        for key in keys:
+            if not key in self.dict or self.dict[key] is None:
+                self.dict[key] = {}
+
+            self.dict = self.dict[key]
+
+        return self.dict
+
+    def reset(self):
+        self.dict = {}
+        self.yaml_to_write = {}
+        self.last_key = ''
